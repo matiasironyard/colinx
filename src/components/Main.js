1a439e95f1974ae74a447980e03059dcec95b549
Matias Mariani <hi@matiasmariani.io>
2017-09-03 00:11:48 -0400

    styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,10 +17,10 @@ class Main extends Component {
 
             <div className="col-xs-12 col-sm-6  col-md-4 col-lg-4">
               <div className="thumbnail">
-                <img className="img-responsive" src="http://0.media.collegehumor.cvcdn.com/57/51/e81c87cc8bda63dd2a770c92463a345b.jpg" alt="..."/>
+                <img className="img-responsive" src="http://0.media.collegehumor.cvcdn.com/57/51/e81c87cc8bda63dd2a770c92463a345b.jpg" alt="$ 10.99"/>
                 <div className="caption">
-                  <h3>Thumbnail label</h3>
-                  <p>...</p>
+                  <h3>Item</h3>
+                  <p>$ 10.99</p>
                   <p>
                     <a href="/" className="btn btn-primary" role="button">Buy</a>
                     <a href="/" className="btn btn-default pull-right" role="button">Details</a>
@@ -31,10 +31,10 @@ class Main extends Component {
 
             <div className="col-xs-12 col-sm-6  col-md-4 col-lg-4 ">
               <div className="thumbnail">
-                <img className="img-responsive" className="img-responsive" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAOzAAAAJDhmM2E1MmM4LTI5MWQtNGRiMC04ZWFjLTQ5NmU0ODU3ODUxNQ.jpg" alt="..."/>
+                <img className="img-responsive" className="img-responsive" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAOzAAAAJDhmM2E1MmM4LTI5MWQtNGRiMC04ZWFjLTQ5NmU0ODU3ODUxNQ.jpg" alt="$ 10.99"/>
                 <div className="caption">
-                  <h3>Thumbnail label</h3>
-                  <p>...</p>
+                  <h3>Item</h3>
+                  <p>$ 10.99</p>
                   <p>
                     <a href="/" className="btn btn-primary" role="button">Buy</a>
                     <a href="/" className="btn btn-default pull-right" role="button">Details</a>
@@ -45,10 +45,10 @@ class Main extends Component {
 
             <div className="col-xs-12 col-sm-6  col-md-4 col-lg-4 ">
               <div className="thumbnail">
-                <img className="img-responsive" src="http://www.goodnet.org/photos/620x0/19236.jpg" alt="..."/>
+                <img className="img-responsive" src="http://www.goodnet.org/photos/620x0/19236.jpg" alt="$ 10.99"/>
                 <div className="caption">
-                  <h3>Thumbnail label</h3>
-                  <p>...</p>
+                  <h3>Item</h3>
+                  <p>$ 10.99</p>
                   <p>
                     <a href="/" className="btn btn-primary" role="button">Buy</a>
                     <a href="/" className="btn btn-default pull-right" role="button">Details</a>
@@ -59,10 +59,10 @@ class Main extends Component {
 
             <div className="col-xs-12 col-sm-6  col-md-4 col-lg-4 ">
               <div className="thumbnail">
-                <img className="img-responsive" src="https://img1.etsystatic.com/141/0/147502834351/inv_fullxfull.1253296721_ebor4lmf.jpg" alt="..."/>
+                <img className="img-responsive" src="https://img1.etsystatic.com/141/0/147502834351/inv_fullxfull.1253296721_ebor4lmf.jpg" alt="$ 10.99"/>
                 <div className="caption">
-                  <h3>Thumbnail label</h3>
-                  <p>...</p>
+                  <h3>Item</h3>
+                  <p>$ 10.99</p>
                   <p>
                     <a href="/" className="btn btn-primary" role="button">Buy</a>
                     <a href="/" className="btn btn-default pull-right" role="button">Details</a>
@@ -73,10 +73,10 @@ class Main extends Component {
 
             <div className="col-xs-12 col-sm-6  col-md-4 col-lg-4 ">
               <div className="thumbnail">
-                <img className="img-responsive" src="https://static1.squarespace.com/static/56abd992d210b84c4cf9fdfb/t/590240641b631b595f848dc6/1493319826000/Wedding+Cards+%7C+Stationery+%7C+Illustration+%7C+Katie+Vaz?format=750w" alt="..."/>
+                <img className="img-responsive" src="https://static1.squarespace.com/static/56abd992d210b84c4cf9fdfb/t/590240641b631b595f848dc6/1493319826000/Wedding+Cards+%7C+Stationery+%7C+Illustration+%7C+Katie+Vaz?format=750w" alt="$ 10.99"/>
                 <div className="caption">
-                  <h3>Thumbnail label</h3>
-                  <p>...</p>
+                  <h3>Item</h3>
+                  <p>$ 10.99</p>
                   <p>
                     <a href="/" className="btn btn-primary" role="button">Buy</a>
                     <a href="/" className="btn btn-default pull-right" role="button">Details</a>
@@ -87,10 +87,10 @@ class Main extends Component {
 
             <div className="col-xs-12 col-sm-6  col-md-4 col-lg-4 ">
               <div className="thumbnail">
-                <img className="img-responsive" src="https://img1.etsystatic.com/067/0/6602097/il_340x270.785321693_fala.jpg" alt="..."/>
+                <img className="img-responsive" src="https://img1.etsystatic.com/067/0/6602097/il_340x270.785321693_fala.jpg" alt="$ 10.99"/>
                 <div className="caption">
-                  <h3>Thumbnail label</h3>
-                  <p>...</p>
+                  <h3>Item</h3>
+                  <p>$ 10.99</p>
                   <p>
                     <a href="/" className="btn btn-primary" role="button">Buy</a>
                     <a href="/" className="btn btn-default pull-right" role="button">Details</a>
@@ -101,10 +101,10 @@ class Main extends Component {
 
             <div className="col-xs-12 col-sm-6  col-md-4 col-lg-4 ">
               <div className="thumbnail">
-                <img className="img-responsive" src="https://i.pinimg.com/736x/f8/e9/fc/f8e9fc99d6c7c0555820f747587f86a4--pumpkin-pumpkin-pumpkin-baby.jpg" alt="..."/>
+                <img className="img-responsive" src="https://i.pinimg.com/736x/f8/e9/fc/f8e9fc99d6c7c0555820f747587f86a4--pumpkin-pumpkin-pumpkin-baby.jpg" alt="$ 10.99"/>
                 <div className="caption">
-                  <h3>Thumbnail label</h3>
-                  <p>...</p>
+                  <h3>Item</h3>
+                  <p>$ 10.99</p>
                   <p>
                     <a href="/" className="btn btn-primary" role="button">Buy</a>
                     <a href="/" className="btn btn-default pull-right" role="button">Details</a>
@@ -115,10 +115,10 @@ class Main extends Component {
 
             <div className="col-xs-12 col-sm-6  col-md-4 col-lg-4 ">
               <div className="thumbnail">
-                <img className="img-responsive" src="http://www.goodnet.org/photos/620x0/19232.jpg" alt="..."/>
+                <img className="img-responsive" src="http://www.goodnet.org/photos/620x0/19232.jpg" alt="$ 10.99"/>
                 <div className="caption">
-                  <h3>Thumbnail label</h3>
-                  <p>...</p>
+                  <h3>Item</h3>
+                  <p>$ 10.99</p>
                   <p>
                     <a href="/" className="btn btn-primary" role="button">Buy</a>
                     <a href="/" className="btn btn-default pull-right" role="button">Details</a>
@@ -129,10 +129,10 @@ class Main extends Component {
 
             <div className="col-xs-12 col-sm-6  col-md-4 col-lg-4 ">
               <div className="thumbnail">
-                <img className="img-responsive" src="https://blog.etsy.com/en/files/2017/08/Zandra_Header-1200x900.jpg" alt="..."/>
+                <img className="img-responsive" src="https://blog.etsy.com/en/files/2017/08/Zandra_Header-1200x900.jpg" alt="$ 10.99"/>
                 <div className="caption">
-                  <h3>Thumbnail label</h3>
-                  <p>...</p>
+                  <h3>Item</h3>
+                  <p>$ 10.99</p>
                   <p>
                     <a href="/" className="btn btn-primary" role="button">Buy</a>
                     <a href="/" className="btn btn-default pull-right" role="button">Details</a>
@@ -143,10 +143,10 @@ class Main extends Component {
 
             <div className="col-xs-12 col-sm-6  col-md-4 col-lg-4 ">
               <div className="thumbnail">
-                <img className="img-responsive" src="https://img1.etsystatic.com/153/0/14640712/il_340x270.1249402985_h5k7.jpg" alt="..."/>
+                <img className="img-responsive" src="https://img1.etsystatic.com/153/0/14640712/il_340x270.1249402985_h5k7.jpg" alt="$ 10.99"/>
                 <div className="caption">
-                  <h3>Thumbnail label</h3>
-                  <p>...</p>
+                  <h3>Item</h3>
+                  <p>$ 10.99</p>
                   <p>
                     <a href="/" className="btn btn-primary" role="button">Buy</a>
                     <a href="/" className="btn btn-default pull-right" role="button">Details</a>
@@ -157,10 +157,10 @@ class Main extends Component {
 
             <div className="col-xs-12 col-sm-6  col-md-4 col-lg-4 ">
               <div className="thumbnail">
-                <img className="img-responsive" src="https://img0.etsystatic.com/163/0/9864230/il_340x270.1200916444_dpip.jpg" alt="..."/>
+                <img className="img-responsive" src="https://img0.etsystatic.com/163/0/9864230/il_340x270.1200916444_dpip.jpg" alt="$ 10.99"/>
                 <div className="caption">
-                  <h3>Thumbnail label</h3>
-                  <p>...</p>
+                  <h3>Item</h3>
+                  <p>$ 10.99</p>
                   <p>
                     <a href="/" className="btn btn-primary" role="button">Buy</a>
                     <a href="/" className="btn btn-default pull-right" role="button">Details</a>
@@ -171,10 +171,10 @@ class Main extends Component {
 
             <div className="col-xs-12 col-sm-6  col-md-4 col-lg-4 ">
               <div className="thumbnail">
-                <img className="img-responsive" src="https://img0.etsystatic.com/114/2/5176706/il_340x270.999493946_4itb.jpg" alt="..."/>
+                <img className="img-responsive" src="https://img0.etsystatic.com/114/2/5176706/il_340x270.999493946_4itb.jpg" alt="$ 10.99"/>
                 <div className="caption">
-                  <h3>Thumbnail label</h3>
-                  <p>...</p>
+                  <h3>Item</h3>
+                  <p>$ 10.99</p>
                   <p>
                     <a href="/" className="btn btn-primary" role="button">Buy</a>
                     <a href="/" className="btn btn-default pull-right" role="button">Details</a>
